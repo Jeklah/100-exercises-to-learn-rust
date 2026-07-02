95a317ff9f05994729a7747f3a4f1bd9069456ce
Jeklah <arthurbowers@gmail.com>
2026-07-02 12:07:07 +0100

    Adding solution  in progress for 04/14_outro.

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,9 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+
